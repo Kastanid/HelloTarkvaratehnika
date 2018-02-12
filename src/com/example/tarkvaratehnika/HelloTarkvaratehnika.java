ec7804d29eb4271d1ef53478cd49c646d0531a75
SiimErik <siimpotsepp@gmail.com>
2018-02-12 21:34:45 +0200

    Update HelloTarkvaratehnika.java

--- src/com/example/tarkvaratehnika/HelloTarkvaratehnika.java
+++ src/com/example/tarkvaratehnika/HelloTarkvaratehnika.java
@@ -2,6 +2,6 @@ package com.example.tarkvaratehnika;
 
 public class HelloTarkvaratehnika {
     public static void main(String[] args){
-        System.out.println("Tere Tarkvaratehnika");
+        System.out.println("Tere Tarkvaratehnika!");
     }
 }
